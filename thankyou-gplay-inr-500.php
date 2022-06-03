b61c81137e3ea4ba5be82c6bbcd8505c2b613e85
DarkShadowLK <106459427+DarkShadowLK@users.noreply.github.com>
2022-06-03 10:10:55 +0530

    Add files via upload

---
+++ thankyou-gplay-inr-500.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Thanks for your order!</title>
+</head>
+<body>
+    <p>Your payment has been successfully received.
+<br>You will receive a confirmation mail shortly along with giftcard.
+    </p>
+</body>
+</html>
